0ebf3d3529bee21723ce1f928a63211c6032f922
Thomas Cowart <tncowart@gmail.com>
2021-12-05 00:33:19 -0500

    Day 3: code cleanup

--- src/day3.rs
+++ src/day3.rs
@@ -1,117 +1,42 @@
 fn vec2bits(vec: &[u32]) -> u32 {
-    let mut result = 0;
-
-    for (i, item) in vec.iter().rev().enumerate() {
-        result |= item << i;
-    }
-
-    result
+    vec.iter()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (i, item)| acc | item << i)
 }
 
 fn most_common_values(lines: &[Vec<u32>]) -> Vec<u32> {
     let mut acc = vec![0; lines[0].len()];
-    let count = (lines.len() / 2) as u32;
-
-    for line in lines {
-        for (i, val) in line.iter().enumerate() {
-            acc[i] += val;
-        }
+    for pos in (0..12).rev() {
+        acc[pos] = most_common_value(lines, pos)
     }
-
-    acc.iter()
-        .map(|i| if i >= &count { 1 } else { 0 })
-        .collect()
+    acc
 }
 
 fn most_common_value(lines: &[Vec<u32>], pos: usize) -> u32 {
-    let mut acc = 0;
-    let count = (lines.len() / 2) as u32;
-
-    for line in lines {
-        acc += line[pos];
-    }
-
-    if acc >= count {
-        1
-    } else {
-        0
-    }
+    let count = ((lines.len() + lines.len() % 2) / 2) as u32;
+    let acc = lines.iter().fold(0, |acc, l| acc + l[pos]);
+    (acc >= count) as u32
 }
 
-fn least_common_value(lines: &[Vec<u32>], pos: usize) -> u32 {
-    let mut acc = 0;
-    let mut count = (lines.len() / 2) as u32;
-    if lines.len() % 2 == 1 {
-        count += 1;
-    }
-
-    for line in lines {
-        acc += line[pos];
-    }
-
-    if acc >= count {
-        0
-    } else {
-        1
-    }
-}
-
-fn _day3(lines: &[Vec<u32>]) -> (u32, u32) {
-    let gamma_vec = most_common_values(lines);
-    let gamma = vec2bits(&gamma_vec);
-
-    let epsilon_vec = gamma_vec
-        .iter()
-        .map(|i| if i == &1 { 0 } else { 1 })
-        .collect::<Vec<u32>>();
-    let epsilon = vec2bits(&epsilon_vec);
-
-    let power_consumption = gamma * epsilon;
-
-    let mut oxygens: Vec<Vec<u32>> = lines.to_vec();
-    let mut oxygen = vec![0; lines[0].len()];
+fn life_support(lines: &[Vec<u32>], most: u32) -> u32 {
+    let mut gases: Vec<Vec<u32>> = lines.to_vec();
+    let mut gas = vec![0; lines[0].len()];
     let mut mask = 12;
 
-    while oxygens.len() != 1 {
+    while gases.len() != 1 {
         mask -= 1;
-        let mut new_oxygens: Vec<Vec<u32>> = Vec::new();
         let pos = 11 - mask;
-        oxygen[pos] = most_common_value(&oxygens, pos);
-        let most_common = vec2bits(&oxygen);
+        gas[pos] = (most_common_value(&gases, pos) == most) as u32;
 
-        for line in oxygens {
-            if most_common >> mask == vec2bits(&line) >> mask {
-                new_oxygens.push(line.clone());
-            }
-        }
-
-        oxygens = new_oxygens;
+        gases = gases
+            .iter()
+            .cloned()
+            .filter(|l| vec2bits(&gas) >> mask == vec2bits(l) >> mask)
+            .collect();
     }
 
-    let mut co2s: Vec<Vec<u32>> = lines.to_vec();
-    let mut co2 = vec![0; lines[0].len()];
-    mask = 12;
-
-    while co2s.len() != 1 {
-        mask -= 1;
-        let mut new_c02s: Vec<Vec<u32>> = Vec::new();
-        let pos = 11 - mask;
-        co2[pos] = least_common_value(&co2s, pos);
-        let least_common = vec2bits(&co2);
-
-        for line in co2s {
-            if least_common >> mask == vec2bits(&line) >> mask {
-                new_c02s.push(line.clone());
-            }
-        }
-
-        co2s = new_c02s;
-    }
-
-    let o2 = vec2bits(&oxygens[0]);
-    let co2 = vec2bits(&co2s[0]);
-
-    (power_consumption, o2 * co2)
+    vec2bits(&gases[0])
 }
 
 pub fn day3() {
@@ -125,7 +50,15 @@ pub fn day3() {
         })
         .collect();
 
-    let x = _day3(&lines);
-    println!("Day 03.1: {}", x.0);
-    println!("Day 03.2: {}", x.1);
+    let gamma = vec2bits(&most_common_values(&lines));
+    let epsilon = !gamma & 0b00000000000000000000111111111111;
+    let power = gamma * epsilon;
+    assert_eq!(2250414, power);
+    println!("Day 03.1: {}", power);
+
+    let o2 = life_support(&lines, 1);
+    let co2 = life_support(&lines, 0);
+    let life = o2 * co2;
+    assert_eq!(6085575, life);
+    println!("Day 03.2: {}", life);
 }
