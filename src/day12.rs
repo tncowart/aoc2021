28611fb16295359580d192b3fbd327a8a733fbaa
Thomas Cowart <tncowart@gmail.com>
2021-12-14 20:27:48 -0500

    un-"cargo fmt"td code
    cargo fmt wants lines too short for my taste

--- src/day12.rs
+++ src/day12.rs
@@ -21,20 +21,16 @@ fn visit_caves<'a>(
         let mut p = path.clone();
         p.push(node);
         let double = if Some(node) == double { None } else { double };
-        (&caves[node] - visited)
-            .iter()
-            .fold(Vec::new(), |mut acc, n| {
-                acc.extend(visit_caves(n, caves, &v, &p, double));
-                acc
-            })
+        (&caves[node] - visited).iter().fold(Vec::new(), |mut acc, n| {
+            acc.extend(visit_caves(n, caves, &v, &p, double));
+            acc
+        })
     }
 }
 
 pub fn day12() {
     let caves = include_str!("../resources/day12.txt")
-        .lines()
-        .filter(|l| !(*l).is_empty())
-        .map(|l| l.split_once('-').unwrap())
+        .lines().filter(|l| !(*l).is_empty()).map(|l| l.split_once('-').unwrap())
         .fold(
             HashMap::<&str, HashSet<&str>>::new(),
             |mut acc, (ra, rb)| {
@@ -44,33 +40,12 @@ pub fn day12() {
             },
         );
 
-    println!(
-        "Day 12.1: {}",
-        visit_caves(
-            "start",
-            &caves,
-            &HashSet::from(["start"]),
-            &Vec::new(),
-            None
-        )
-        .len()
-    );
+    println!("Day 12.1: {}", visit_caves("start", &caves, &HashSet::from(["start"]), &Vec::new(), None).len());
 
-    println!(
-        "Day 12.2: {}",
-        caves
-            .keys()
-            .filter(|k| is_lower(k) && **k != "end" && **k != "start")
+    println!("Day 12.2: {}", caves.keys().filter(|k| is_lower(k) && **k != "end" && **k != "start")
             .fold(HashSet::new(), |mut acc, k| {
-                acc.extend(visit_caves(
-                    "start",
-                    &caves,
-                    &HashSet::from(["start"]),
-                    &Vec::new(),
-                    Some(k),
-                ));
+                acc.extend(visit_caves("start", &caves, &HashSet::from(["start"]), &Vec::new(), Some(k)));
                 acc
-            })
-            .len()
+            }).len()
     );
 }
