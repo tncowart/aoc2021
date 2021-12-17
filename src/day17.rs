dae9497e537c811f1021a045f9e757dd7a397e15
Thomas Cowart <tncowart@gmail.com>
2021-12-17 11:09:35 -0500

    Day 17, a little cleanup

--- src/day17.rs
+++ src/day17.rs
@@ -1,18 +1,17 @@
-fn launch(x1: i32, x2: i32, y1: i32, y2: i32) -> (i32, i32, i32) {
+fn launch(x1: i32, x2: i32, y1: i32, y2: i32) -> (i32, i32) {
     let mut max_y = i32::MIN;
-    let mut max_y_x = 0;
     let mut total = 0;
     for x in 0..=(x2 + 1) {
-        for y in y2..1000 {
+        for y in y2..500 {
             let mut curr_x = x;
             let mut curr_y = y;
             let mut pos_x = 0;
             let mut pos_y = 0;
             while pos_x <= x2 && pos_y >= y2 {
+                // If the current position is in the target area
                 if pos_x >= x1 && pos_x <= x2 && pos_y <= y1 && pos_y >= y2 {
                     if y > max_y {
                         max_y = y;
-                        max_y_x = x;
                     }
                     total += 1;
                     break;
@@ -24,12 +23,12 @@ fn launch(x1: i32, x2: i32, y1: i32, y2: i32) -> (i32, i32, i32) {
             }
         }
     }
-    (max_y_x, max_y, total)
+    (max_y, total)
 }
 
 pub fn day17() {
     // target area: x=34..67, y=-215..-186
-    let (_, y, total) = launch(34, 67, -186, -215);
+    let (y, total) = launch(34, 67, -186, -215);
     println!("Day 17.1: {}", (y*(y+1)) / 2);
     println!("Day 17.2: {}", total);
 }
