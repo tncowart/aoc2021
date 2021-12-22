8e2b7b6bf16d68ef87620c3c91aa8d94c4f8d102
Thomas Cowart <tncowart@gmail.com>
2021-12-22 07:21:26 -0500

    Removes "universe" argument
    The universe count is calculated "on the way up" instead of on the
    way down.

--- src/day21.rs
+++ src/day21.rs
@@ -31,9 +31,9 @@ fn dirac1(p1_start: u32, p2_start: u32) -> (u32, u32, u32) {
     (p1_score, p2_score, roll_ct)
 }
 
-fn dirac2(p1_pos: u64, p1_score: u64, p2_pos: u64, p2_score: u64, universes: u64) -> (u64, u64) {
+fn dirac2(p1_pos: u64, p1_score: u64, p2_pos: u64, p2_score: u64) -> (u64, u64) {
     if p2_score >= P2_GOAL {
-        return (universes, 0)
+        return (0, 1)
     }
 
     let p1_pos3 = (((p1_pos - 1) + 3) % BOARD_SIZE as u64) + 1;
@@ -43,20 +43,20 @@ fn dirac2(p1_pos: u64, p1_score: u64, p2_pos: u64, p2_score: u64, universes: u64
     let p1_pos7 = (((p1_pos - 1) + 7) % BOARD_SIZE as u64) + 1;
     let p1_pos8 = (((p1_pos - 1) + 8) % BOARD_SIZE as u64) + 1;
     let p1_pos9 = (((p1_pos - 1) + 9) % BOARD_SIZE as u64) + 1;
-    let (p23, p13) = dirac2(p2_pos, p2_score, p1_pos3, p1_score + p1_pos3, universes);
-    let (p24, p14) = dirac2(p2_pos, p2_score, p1_pos4, p1_score + p1_pos4, universes * 3);
-    let (p25, p15) = dirac2(p2_pos, p2_score, p1_pos5, p1_score + p1_pos5, universes * 6);
-    let (p26, p16) = dirac2(p2_pos, p2_score, p1_pos6, p1_score + p1_pos6, universes * 7);
-    let (p27, p17) = dirac2(p2_pos, p2_score, p1_pos7, p1_score + p1_pos7, universes * 6);
-    let (p28, p18) = dirac2(p2_pos, p2_score, p1_pos8, p1_score + p1_pos8, universes * 3);
-    let (p29, p19) = dirac2(p2_pos, p2_score, p1_pos9, p1_score + p1_pos9, universes);
-    (p13 + p14 + p15 + p16 + p17 + p18 + p19, p23 + p24 + p25 + p26 + p27 + p28 + p29)
+    let (p23, p13) = dirac2(p2_pos, p2_score, p1_pos3, p1_score + p1_pos3);
+    let (p24, p14) = dirac2(p2_pos, p2_score, p1_pos4, p1_score + p1_pos4);
+    let (p25, p15) = dirac2(p2_pos, p2_score, p1_pos5, p1_score + p1_pos5);
+    let (p26, p16) = dirac2(p2_pos, p2_score, p1_pos6, p1_score + p1_pos6);
+    let (p27, p17) = dirac2(p2_pos, p2_score, p1_pos7, p1_score + p1_pos7);
+    let (p28, p18) = dirac2(p2_pos, p2_score, p1_pos8, p1_score + p1_pos8);
+    let (p29, p19) = dirac2(p2_pos, p2_score, p1_pos9, p1_score + p1_pos9);
+    (p13 + 3 * p14 + 6 * p15 + 7 * p16 + 6 * p17 + 3 * p18 + p19, p23 + 3 * p24 + 6 * p25 + 7 * p26 + 6 * p27 + 3 * p28 + p29)
 }
 
 pub fn day21() {
     let (p1_score, p2_score, roll_ct) = dirac1(6, 3);
     println!("Day 21.1: {}", p1_score.min(p2_score) * roll_ct);
 
-    let (p1_wins, p2_wins) = dirac2(6, 0, 3, 0, 1);
+    let (p1_wins, p2_wins) = dirac2(6, 0, 3, 0);
     println!("Day 21.2: {}-{}", p1_wins, p2_wins);
 }
